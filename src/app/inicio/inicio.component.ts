f79b05733bae4058def50c40af6fcde5f2cfd770
Alan Fagundes <alanfagundes184@gmail.com>
2025-08-02 15:11:20 -0300

    projrto api angular

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from '../produto.service';
+import { Produto } from '../models/produto.models';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
 
+  public produtos: Produto[] = [];
+
+  constructor(private _produtoService: ProdutoService) {}
+
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+
+  listarProdutos(): void {
+    this._produtoService.getProdutos().subscribe(
+       retornoProduto =>{
+         this.produtos = retornoProduto.map(
+        item => {
+        return new Produto(
+          item.id,
+          item.produto,
+          item.descricao,
+          item.foto,
+          item.preco
+        );
+      });
+    });
+  }
 }
